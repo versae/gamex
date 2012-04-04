430a8a41f05c19a4b25c8d1b53d6643f3e5f1cfe
Roberto Ulloa <roberto@roberto-HP-G62-Notebook-PC.(none)>
2012-04-04 01:15:30 -0400

    controlling the events on images manually

--- gamex.py
+++ gamex.py
@@ -11,6 +11,7 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.core.window import Window
 
 from functools import partial
 import settings
@@ -27,57 +28,97 @@ class Controller(FloatLayout):
     Add an action to be called from the kv lang file.
     '''
 
+    # Type of actions
+    FACE = 'face'
+    EYES = 'eyes'
+    EARS = 'ears'
+    NOSE = 'nose'
+    THROAT = 'throat'
+    MOUTH = 'mouth'
+    selected = FACE
+
+    # Information of the action
+    actions = {'face':'img/punch.png',
+                'eyes':'img/icon.png',
+                'ears':'img/punch.png',
+                'nose':'img/punch.png',
+                'throat':'img/punch.png',
+                'mouth':'img/punch.png'}
+
+    # References to the database
     db = backend.get("metadata")
     index =  0
     count = db.count() - 1
+
+    # references to the paint widget of the interface
     paint = ObjectProperty(None)
-    menu = ObjectProperty(None)
-    colors = {'tree':[1,0,0,0.4],
-                'alt':[0,1,0,0.2],
-                'alt2':[0,0,1,0.4],
-                'profile':[0.8,0.8,0,0.4],
-                'user': [0,0.8,0.8, 0.4]}
+    controls = ObjectProperty(None)
+
+    # keeps the possition of the current image
     x_start = 0
     y_start = 0
     prop = 1.0
-    ellipses = []
+    
+    # these are the ellipses of the current faces 
+    faces = []
+
+    # Variables for scores
     score = 0
     high_score = 0
-    icon = None
 
-    def show_faces(self):
-        with self.paint.canvas:
-            r = self.db.get(self.index)['resolution']
+    def __init__(self, **kwargs):
+        """ Overridint the constructor """
+        super(Controller, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
-            if r['width'] <= self.paint.width \
-                and r['height'] <= self.paint.height:
-                self.prop = 1.0
-            elif self.paint.width - r['width'] < self.paint.height - r['height']:
-                self.prop = self.paint.width / float(r['width'])
-            else:
-                self.prop = self.paint.height / float(r['height'])
+    def touch_down(self, paint, touch, *args):
+        """ Controls when there is a touch in the screen """
+        print "original: "+ str(touch.x) + ',' + str(touch.y)
+        
+        touch = self.to_image_coord(touch)
+        touch = self.to_screen_coord(touch)
+        if self.paint.collide_point(touch.x, touch.y):
+            w=h=48
+            icon = Image(source= self.actions[self.selected], 
+                            pos=(touch.x-int(w/2.0),touch.y-int(h/2.0)), 
+                            size=(w,h))
+            self.paint.add_widget(icon);
+            Clock.schedule_interval(partial(fade, parent = self, image = icon), .01)
+        else:
+            for ctrl in self.controls.children:
+                if ctrl.collide_point(touch.x, touch.y):
+                    self.set_selected(ctrl, ctrl.action)
 
-            self.x_start=self.paint.width/2.0 - \
-                    self.paint.norm_image_size[0] / 2.0
-            self.y_start=self.paint.height/2.0 - \
-                            self.paint.norm_image_size[1] / 2.0
+    def previous(self):
+        """ shows the previous image and update the index """
+        self.hide_faces()
+        if self.index > 0:
+            self.index -= 1
+        else:
+            self.index == self.count
+        self.paint.source = self.db.get(self.index)["image"]
+        self.show_faces()
 
-            self.ellipses = []
-            for k,faces in self.db.get(self.index)['face_methods'].iteritems():
-                for f in faces:
-                    print 'face: ' + str(f)
-                    Color(  self.colors[k][0],self.colors[k][1],
-                            self.colors[k][2],self.colors[k][3] )
-                    e = Ellipse(
-                            pos =(  self.x_start + int(f['x']*self.prop) - 
-                                    int(f['width'] * self.prop / 2.0),
-                                    self.height - self.y_start - 
-                                    int(f['y']*self.prop) - 
-                                    int(f['height'] * self.prop/2.0)), 
-                            size =( int(f['width'] * self.prop),
-                                    int(f['height'] * self.prop)))
-                    print 'ellipse: ' + str(e.pos)
-                    self.ellipses.append(e)
+    def set_selected(self, btn, select):
+        """ set the type of game """
+        self.unselect()
+        self.selected = select
+        btn.children[0].color = [1,1,1,1]
+
+    def unselect(self):
+        for c in self.controls.children:
+            c.children[0].color = [.5,.5,.5,.5]
+
+    def next(self):
+        """ shows the next image and update the index """
+        self.hide_faces()
+        if self.index < self.count:
+            self.index += 1
+        else:
+            self.index = 0
+        self.paint.source = self.db.get(self.index)["image"]
+
 
     def to_image_coord(self, touch):
         touch.x = int((touch.x - self.x_start) / float(self.prop))
@@ -91,58 +132,60 @@ class Controller(FloatLayout):
         print "to_screen: "+ str(touch.x) + ',' + str(touch.y)
         return touch
 
-    def remove_ellipses(self):
+    def _keyboard_closed(self):
+        """ controls when the keyboard is closed"""
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        """ with this is possible to control to show/hidden the faces """
+        if keycode[1] == 's':
+            self.show_faces()
+        if keycode[1] == 'h':
+            self.hide_faces()
+        return True
+
+    def hide_faces(self):
+        """ hide the faces """
         with self.paint.canvas:
-            for e in self.ellipses:
-                self.paint.canvas.remove(e)
+            for f in self.faces:
+                self.paint.canvas.remove(f)
+            self.faces = []
 
-    def touch_down(self, paint, touch, *args):
-        print "original: "+ str(touch.x) + ',' + str(touch.y)
-        touch = self.to_image_coord(touch)
-        touch = self.to_screen_coord(touch)
-        w=h=48
-        icon = Image(source='img/punch.png', 
-                        pos=(touch.x-int(w/2.0),touch.y-int(h/2.0)), 
-                        size=(w,h))
-        self.paint.add_widget(icon);
-        Clock.schedule_interval(partial(fade, parent = self, image = icon), .01)
-
-
-    def start(self):
-        self.remove_widget(self.menu)
-        self.paint.source = self.db.get(self.index)["image"]
-        self.show_faces()
-
-    def previous(self):
-        """ shows the previous image and update the index """
-        self.remove_ellipses()
-        if self.index > 0:
-            self.index -= 1
-        else:
-            self.index == self.count
-        self.paint.source = self.db.get(self.index)["image"]
-        self.show_faces()
-
-    def face(self):
-        pass
-
-    def eyes(self):
-        pass
-
-    def next(self):
-        """ shows the next image and update the index """
-        self.remove_ellipses()
-        if self.index < self.count:
-            self.index += 1
-        else:
-            self.index = 0
-        self.paint.source = self.db.get(self.index)["image"]
-        self.show_faces()
+    def show_faces(self):
+        """ show the faces from the database """
+        with self.paint.canvas:
+            r = self.db.get(self.index)['resolution']
+            if r['width'] <= self.paint.width \
+                and r['height'] <= self.paint.height:
+                self.prop = 1.0
+            elif self.paint.width - r['width'] < self.paint.height - r['height']:
+                self.prop = self.paint.width / float(r['width'])
+            else:
+                self.prop = self.paint.height / float(r['height'])
+            self.x_start=self.paint.width/2.0 - \
+                    self.paint.norm_image_size[0] / 2.0
+            self.y_start=self.paint.height/2.0 - \
+                            self.paint.norm_image_size[1] / 2.0
+            for k,faces in self.db.get(self.index)['face_methods'].iteritems():
+                for f in faces:
+                    print 'face: ' + str(f)
+                    Color( random.random(), random.random(), 
+                            random.random(), 0.5)
+                    e = Ellipse(
+                            pos =(  self.x_start + int(f['x']*self.prop) - 
+                                    int(f['width'] * self.prop / 2.0),
+                                    self.height - self.y_start - 
+                                    int(f['y']*self.prop) - 
+                                    int(f['height'] * self.prop/2.0)), 
+                            size =( int(f['width'] * self.prop),
+                                    int(f['height'] * self.prop)))
+                    print 'ellipse: ' + str(e.pos)
+                    self.faces.append(e)
 
 
 def fade(time, parent, image, rate = .01, limit = 0):
     a = image.color[3]
-
     if a > limit:
         a -= rate
         image.color = (1, 1, 1, a)
@@ -153,7 +196,6 @@ def fade(time, parent, image, rate = .01, limit = 0):
 
 
 class ControllerApp(App):
-
     def build(self):
         return Controller(info='Hello world')
 
